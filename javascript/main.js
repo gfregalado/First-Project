2e755609aabee4614da135f4335dc1ebd3fe038d
Gonçalo Regalado <gf.regalado@gmail.com>
2019-10-22 21:58:25 +0100

    created the engine. Added an image, basic movement and animations

--- javascript/main.js
+++ javascript/main.js
@@ -1,13 +1,55 @@
-let myGameArea = {
-  canvas: document.createElement("canvas"),
-  frames: 0,
-  start: function() {
-    this.canvas.width = 560;
-    this.canvas.height = 320;
-    this.context = this.canvas.getContext("2d");
-    this.canvas.classList.add("canvasBg");
-    document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+// Defining the Canvas //
+let $canvas = document.getElementById("myCanvas");
+let ctx = $canvas.getContext("2d");
+
+class player {
+  constructor(positionX, positionY) {
+    this.positionX = positionX;
+    this.positionY = positionY;
+    this.frames = 0;
+  }
+
+  renderIdlePlayer() {
+    let img = new Image();
+    img.src = "/Images/hero/Idle/0_Reaper_Man_Idle_" + this.frames + ".png";
+    ctx.drawImage(img, this.positionX, this.positionY - 56, 64, 64);
+    this.frames++;
+    if (this.frames === 17) {
+      this.frames = 0;
+    }
+  }
+}
+
+// Creating a Player //
+
+let mario = new player(32, $canvas.height);
+
+// Movement and behaviour //
+
+// Controls //
+
+document.onkeydown = function(e) {
+  switch (e.keyCode) {
+    case 38: // up arrow
+      mario.positionY -= 5;
+      console.log(mario.positionY);
+      break;
+    case 37: // left arrow
+      mario.positionX -= 5;
+      console.log(mario.positionX);
+      break;
+    case 39: // right arrow
+      mario.positionX += 5;
+      console.log(mario.positionX);
+      break;
   }
 };
 
-myGameArea.start();
+// Constructor for creating Components //
+
+function updateCanvas() {
+  ctx.clearRect(0, 0, $canvas.width, $canvas.height);
+  mario.renderIdlePlayer();
+}
+
+setInterval(updateCanvas, 20);
